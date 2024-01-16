02ba84030d0dd295941e5b96dff33a84abb6e9cf
GinusHR <1074730@hr.nl>
2024-01-16 12:21:08 +0100

    footer position fixed, and alternate nav bar made

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+<footer>
+    <div class="footerdiv">
+        <div>
+            <div>
+                www.viktoriaschoonmaakbedrijf.com
+            </div>
+            <div>
+                Neem contact op: nummer
+            </div>
+        </div>
+        <div>
+            <div class="linkdiv">
+                <p> Meer weten? <a href="overOns.php">Over ons</a></p>
+            </div>
+        </div
+</footer>
+>
+</div>
