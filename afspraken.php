a9adb443a572e0fb175dd50e73211455aa1fc269
GinusHR <1074730@hr.nl>
2024-01-18 11:40:26 +0100

    added afspraken.css

--- afspraken.php
+++ afspraken.php
@@ -9,6 +9,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles/afspraken.css">
     <link rel="stylesheet" href="styles/style.css">
     <link rel="stylesheet" href="styles/login.css">
     <title>Viktoria Schoonmaakbedrijf - Afspraken</title>
@@ -21,21 +22,26 @@
     </header>
 <div class="formdiv">
     <form action="" method="post">
-        <div>
-            <label for="name">Naam</label>
-        </div>
 
         <div>
-            <input id="name" name="name" type="text" required>
+
+            <div>
+                <label for="name">Naam</label>
+            </div>
+
+            <div>
+                <input id="name" name="name" type="text" required>
+            </div>
+
+            <div>
+                <label for="email">Email</label>
+            </div>
+
+            <div>
+                <input id="email" name="email" type="email" required>
+            </div>
         </div>
 
-        <div>
-            <label for="email">Email</label>
-        </div>
-
-        <div>
-            <input id="email" name="email" type="email" required>
-        </div>
 
 
 
