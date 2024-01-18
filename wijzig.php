5e126fc16a285c43f6ce99ba6ee6fab4f658e40f
TimoSchmidtt <timos.minilaptop@gmail.com>
2024-01-18 12:31:46 +0100

    added wijzig.css updated wijzig.php to add user functionality

--- wijzig.php
+++ wijzig.php
@@ -1,5 +1,31 @@
 <?php
+if(!isset($_SESSION))
+{
+    session_start();
+}
+if(!isset($_SESSION['user']))
+{
+    header("Location: login.php?location=wijzig.php");
+    exit;
+}
 
+require_once 'includes/database.php';
+/** @var $db */
+
+$email = $_SESSION['user']['email'];
+
+$query = "SELECT id FROM users WHERE email = '$email'";
+$result = mysqli_query($db, $query);
+$user_id = mysqli_fetch_assoc($result)['id'];
+
+if(!$user_id)
+{
+    //TODO: add stuff for when user exists but user_id does not.
+}
+
+$query = "SELECT * FROM dates where user_id = '$user_id'";
+$result = mysqli_query($db, $query);
+$dates = mysqli_fetch_all($result);
 ?>
 
 <!doctype html>
@@ -11,6 +37,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="styles/style.css">
     <link rel="stylesheet" href="styles/login.css">
+    <link rel="stylesheet" href="styles/wijzig.css">
     <title>Viktoria Schoonmaakbedrijf - Wijzig afspraken</title>
 </head>
 <body>
@@ -19,11 +46,40 @@
     include_once 'includes/nav.php';
     ?>
 </header>
-<div class="bigtext">
-    <div>
-        <p>Wijzig afspraken</p>
+
+<main class="wijzigMain">
+
+    <div class="bigtext">
+        <div>
+            <p>Wijzig afspraken</p>
+        </div>
     </div>
-</div>
+    <?php if(!empty($dates)): ?>
+    <table class="wijzigTable">
+        <thead>
+            <tr>
+                <th>Date</th>
+                <th>Time</th>
+                <th>Location</th>
+                <th colspan="1"></th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($dates as $date):
+            $datetime = explode(" ", $date[4]);?>
+            <tr>
+                <th><?php echo $datetime[0]?></th>
+                <th><?php echo substr($datetime[1], 0, -3)?></th>
+                <th><?php echo $date[2]?></th>
+                <th><a href="delete.php?<?php echo $date[0]?>">Cancel</a></th>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+    <?php endif; ?>
+
+    <button class="form-knop">Wijzig...</button>
+</main>
 
 <footer>
     <?php
