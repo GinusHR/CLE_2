e41eb0a14d96f1bb5ba771bf2b138676604cf0b2
GinusHR <1074730@hr.nl>
2024-01-18 10:23:18 +0100

    did something

--- login.php
+++ login.php
@@ -37,7 +37,7 @@ if (isset($_POST['submit'])) {
                         'email' => $user['email'],
                     ];
                     $login = true;
-                    header('Location: index.php');
+                    header('Location:index.php');
                 } else {
                     $errors['loginFailed'] = 'Incorrect login credentials';
                 }
