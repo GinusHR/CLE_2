a0c008e0e9b436f995cbb3e818f36ef19b89a5a4
Kashyn <seemlinsky@gmail.com>
2024-01-19 12:57:00 +0100

    Contact page geupdate

---
+++ contact.php
@@ -0,0 +1,49 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles/afspraken.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/login.css">
+    <title>Contact</title>
+</head>
+<body>
+<header>
+    <?php
+    include_once 'includes/nav.php';
+    ?>
+</header>
+<main>
+    <div>
+        <section>
+            <h1 class="bigtext">Contact</h1>
+            <div class="introOverOns">
+                <h1>Vragen? Wij horen graag van je!</h1>
+                <p>Bij Viktoria Schoonmaakbedrijf streven we naar een perfecte balans: hoge kwaliteit voor een lage kwantiteit. Jouw feedback is onze leidraad voor voortdurende verbetering. Neem gerust contact met ons op en ervaar de schoonheid van ongeëvenaarde service!</p>
+                <p>
+                    ✉️ dijana-@live.nl
+                </p>
+                <p>️🗨️️ We staan altijd voor je klaar, 24/7 ondersteuning!  Onze ondersteuning is zelfs in het weekend beschikbaar!</p>
+                <p>📞 +31648267306
+                </p>
+            </div>
+
+        </section>
+    </div>
+</main>
+</body>
+<footer>
+    <?php
+    include_once 'includes/footer.php';
+    ?>
+</footer>
+<style>
+    .introOverOns {
+        background-color: #E4DBD2;
+        margin: 0 15vw;
+        padding: 2vw;
+        border: #E4DBD2 1px solid;
+        border-radius: 15px;
+    }
+</style>
