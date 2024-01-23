d5b721380b34a7eb71333a06f1ceb4d8a6a005b5
GinusHR <1074730@hr.nl>
2024-01-23 10:16:56 +0100

    pagina titles veranderd

--- agenda.php
+++ agenda.php
@@ -34,7 +34,7 @@ mysqli_close($db);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Victoria Schoonmaakbedrijf - Agenda</title>
     <link rel="stylesheet" href="styles/style.css">
     <link rel="stylesheet" href="styles/login.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
--- overOns.php
+++ overOns.php
@@ -11,7 +11,7 @@ session_start();
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Viktoria Schoonmaakbedrijf- About us</title>
+    <title>Viktoria Schoonmaakbedrijf - About us</title>
     <link rel="stylesheet" href="styles/style.css">
     <link rel="stylesheet" href="styles/login.css">
     <style>
